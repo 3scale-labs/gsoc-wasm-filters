676fded507bd779da726ea88b5cd764dc4a9e074
Rahul Anand <rahulanand16nov@gmail.com>
2021-05-26 20:54:53 +0530

    Added structs required for caching

---
+++ threescale/src/cache.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+pub enum Period {
+  Minute,
+  Hour,
+  Day,
+  Week,
+  Month,
+  Year,
+  Eternity,
+}
+
+#[allow(dead_code)]
+pub struct PeriodWindow {
+  start: Duration,
+  end: Duration,
+  window_type: Period,
+}
+
+#[allow(dead_code)]
+pub struct UsageReport {
+  period_window: PeriodWindow,
+  current_value: u32,
+  max_value: u32,
+}
+
+#[allow(dead_code)]
+pub struct Application<'a> {
+  app_id: String,
+  service_id: String,
+  timestamp: Duration,
+  local_state: RefCell<HashMap<&'a str,UsageReport>>,
+  remote_state: RefCell<HashMap<&'a str,UsageReport>>,
+  metric_hierarchy: RefCell<HashMap<&'a str,&'a str>>,
+  unlimited_counter: RefCell<HashMap<&'a str,u32>>,
+}
