f97161a5a5351f8bd26e70d9dcfbcd469e5e8610
Rahul Anand <rahulanand16nov@gmail.com>
2021-07-06 17:50:33 +0530

    integration-tests/api_id_test: use helper funcs to initialize and delete 3scale state

--- integration-tests/app_id_test.go
+++ integration-tests/app_id_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -13,14 +12,57 @@ import (
 
 type AppCredentialTestSuite struct {
 	suite.Suite
+	service_id string
+	service_token string
+	app_id string
+	app_key string
+	user_key string
+	plan_id string
+	metrics []Metric
 }
 
 func (suite *AppCredentialTestSuite) SetupSuite() {
-	err := StartContainers("./configs/app-id/docker-compose.yaml")
-	time.Sleep(10 * time.Second)
-	if err != nil {
+	if err := StartContainers("./configs/app-id/docker-compose.yaml"); err != nil {
 		fmt.Printf("Error: %v", err)
 		suite.Errorf(err, "Error starting docker-compose: %v")
+	}
+	// Initializing 3scale backend state
+	suite.app_id = "test_app_id"
+	suite.app_key = "test_app_key"
+	suite.user_key = "test_user_key"
+	suite.service_id = "test_service_id"
+	suite.plan_id = "test_plan_id"
+	suite.service_token = "test_service_token"
+	suite.metrics = []Metric {
+		Metric {"hits", "1", []UsageLimit {
+			{ Day, 10000,},
+		}},
+		Metric {"rq", "2", []UsageLimit {
+			{ Month, 10000, },
+		}},
+	}
+	if err := CreateService(suite.service_id, suite.service_token); err != nil {
+		suite.Errorf(err, "Error creating a service: %v")
+		return
+	}
+	if err := AddApplication(suite.service_id, suite.app_id, suite.plan_id); err != nil {
+		suite.Errorf(err, "Error adding an application: %v")
+		return
+	}
+	if err := AddUserKey(suite.service_id, suite.app_id, suite.user_key); err != nil {
+		suite.Errorf(err, "Error adding a user key: %v")
+	}
+	if err := AddApplicationKey(suite.service_id, suite.app_id, suite.app_key); err != nil {
+		suite.Errorf(err, "Error adding application key: %v")
+		return
+	}
+	if err := AddMetrics(suite.service_id, &suite.metrics); err != nil {
+		suite.Errorf(err, "Error adding metrics: %v")
+		return
+	}
+	if err := UpdateUsageLimits(suite.service_id, suite.plan_id, &suite.metrics); err != nil {
+		suite.Errorf(err, "Error updating usage limits: %v")
+		return
 	}
 }
 
@@ -30,6 +72,25 @@ func TestAppCredentialSuite(t *testing.T) {
 }
 
 func (suite *AppCredentialTestSuite) TearDownSuite() {
+	fmt.Println("Cleaning 3scale backend state")
+	if err := DeleteService(suite.service_id, suite.service_token); err != nil {
+		suite.Errorf(err, "Failed to delete service: %v")
+	}
+	if err := DeleteApplication(suite.service_id, suite.app_id); err != nil {
+		suite.Errorf(err, "Failed to delete applications: %v")
+	}
+	if err := DeleteApplicationKey(suite.service_id, suite.app_id, suite.app_key); err != nil {
+		suite.Errorf(err, "Failed to delete Application key: %v")
+	}
+	if err := DeleteUserKey(suite.service_id, suite.app_id, suite.user_key); err != nil {
+		suite.Errorf(err, "Failed to delete Application's user key: %v")
+	}
+	if err := DeleteMetrics(suite.service_id, &suite.metrics); err != nil {
+		suite.Errorf(err, "Failed to delete metrics: %v")
+	}
+	if err := DeleteUsageLimits(suite.service_id, suite.plan_id, &suite.metrics); err != nil {
+		suite.Errorf(err, "Failed to delete usage limits")
+	}
 	fmt.Println("Stopping AppCredentialTestSuite")
 	errStop := StopContainers("./configs/app-id/docker-compose.yaml")
 	if errStop != nil {
@@ -43,8 +104,8 @@ func (suite *AppCredentialTestSuite) TestAppIdSuccess() {
 	require.Nilf(suite.T(), errReq, "Error creating the HTTP request: %v", errReq)
 	req.Header = http.Header{
 		"Host":      []string{"localhost"},
-		"x-app-id":  []string{"23f118be"},
-		"x-app-key": []string{"44d128988763aee1b0ff0691f9686f7e"},
+		"x-app-id":  []string{suite.app_id},
+		"x-app-key": []string{suite.app_key},
 	}
 	res, errHTTP := client.Do(req)
 	require.Nilf(suite.T(), errHTTP, "Error sending the HTTP request: %v", errHTTP)
@@ -58,8 +119,8 @@ func (suite *AppCredentialTestSuite) TestAppIdForbidden() {
 	require.Nilf(suite.T(), errReq, "Error creating the HTTP request: %v", errReq)
 	req.Header = http.Header{
 		"Host":      []string{"localhost"},
-		"x-app-id":  []string{"23f118bf"},
-		"x-app-key": []string{"44d128988763aee1b0ff0691f9686f7e"},
+		"x-app-id":  []string{"wrong_app_id"},
+		"x-app-key": []string{suite.app_key},
 	}
 	res, errHTTP := client.Do(req)
 	require.Nilf(suite.T(), errHTTP, "Error sending the HTTP request: %v", errHTTP)
@@ -82,7 +143,7 @@ func (suite *AppCredentialTestSuite) TestUserKeySuccess() {
 
 func (suite *AppCredentialTestSuite) TestUserKeyForbidden() {
 	client := &http.Client{}
-	req, errReq := http.NewRequest("GET", "http://127.0.0.1:9095/", nil)
+	req, errReq := http.NewRequest(http.MethodGet, "http://127.0.0.1:9095/", nil)
 	require.Nilf(suite.T(), errReq, "Error creating the HTTP request: %v", errReq)
 	q := req.URL.Query()
 	q.Add("api_key", "04fa57ba1e465fb53fddc21ead8a7fc1")
