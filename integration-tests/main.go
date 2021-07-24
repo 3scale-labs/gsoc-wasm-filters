409c4bd7730ccefc1d49519689b57d6c873d1bf5
Rahul Anand <rahulanand16nov@gmail.com>
2021-07-24 19:54:05 +0530

    integration: add func to generate a config using the template

--- integration-tests/main.go
+++ integration-tests/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"text/template"
 	"time"
 )
 
 func main() {
 	fmt.Printf("Test package")
+}
+
+// GenerateConfig uses a config template to generate a config file for the proxy.
+func GenerateConfig(name string, jsonData []byte) error {
+	tmpl, err := template.ParseFiles("./config_template.yaml")
+	if err != nil {
+		fmt.Printf("Failed to parse config template: %v", err)
+	}
+
+	out := new(bytes.Buffer)
+	dataHolder := map[string]interface{}{}
+	if err := json.Unmarshal([]byte(jsonData), &dataHolder); err != nil {
+		fmt.Printf("Error parsing the json data provided: %v", err)
+	}
+	if err := tmpl.Execute(out, dataHolder); err != nil {
+		fmt.Printf("Failed to fill-in data in the template: %v", err)
+	}
+
+	if writeErr := ioutil.WriteFile(name, out.Bytes(), 0777); writeErr != nil {
+		fmt.Printf("Error writing temp config file: %v", writeErr)
+	}
+	return nil
 }
 
 // StartProxy build and starts the proxy in a docker container.
@@ -20,7 +46,7 @@ func StartProxy(dockerfile string, envoy string) error {
 		fmt.Printf("Error starting proxy container: %v", err)
 		return err
 	}
-	time.Sleep(10 * time.Second)
+	time.Sleep(15 * time.Second)
 	return nil
 }
 
