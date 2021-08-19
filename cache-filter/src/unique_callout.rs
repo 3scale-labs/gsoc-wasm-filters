334236aa30965ab9925949621813b5e9b75db42a
Rahul Anand <rahulanand16nov@gmail.com>
2021-08-19 19:21:19 +0530

    cache/unique-callout: add better comments for maintainability

--- cache-filter/src/unique_callout.rs
+++ cache-filter/src/unique_callout.rs
@@ -5,8 +5,38 @@ use proxy_wasm::{
 };
 use threescale::proxy::CacheKey;
 
+/** TD;LR on how lock is acquired by exploiting host implementation:
+* cache is essentially a hashmap that maps key to a pair of (value, cas).
+* set_shared_data(key, value, cas)'s psuedo-code is as follows:
+*   lock_using_a_mutex() // This guarantees only 1 thread executes its instructions at a time.
+*   if (key is present in the hashmap)
+*      if (cas && cas != cas_of_last_value_added)
+*          return CasMisMatch
+*      update value with new cas
+*   else insert_new_entry_into_the_hashmap
+*
+* A lock is acquired by a thread when it successfully adds an entry in the cache of the format:
+*              ("callout_{CacheKey}", ({THREAD_ID/ROOT_CONTEXT_ID}, CAS))
+
+* If you look at Rust SDK spec, when a cache entry is not present, you get (None, None) as result.
+* Now imagine a scenario, when two threads (T1 & T2) check for the same lock in the cache and find no lock.
+* T1 & T2 tries to set an entry by calling: set_shared_data(key, value, **None**).
+* If you go through above-mentioned algo, no matter the order which thread executes first, second thread
+* will overwrite the first one because None CAS is translated into 0 in SDK and second-if condition is passed-over.
+
+* Now imagine if T1 & T2 use '1'(any but 0) as CAS, first thread will insert a new entry since it's not 
+* already present in the hashmap and second one will get CasMismatch in the result due to second-if condition.
+* NOTE: Since CAS is a u32 integer, after u32::MAX, CAS resets to 1. What this means for unique-callout
+* is that N threads can successfully acquire the lock again when lock was freed with CAS=1. But the chances
+* of this happening is worse than winning a lottery and proxy can handle multiple callouts.
+
+* It's good to point that 'None' as CAS was intended for overwriting the exisiting value and that's how
+* we free the lock actually, by sending 'None' as value and CAS.
+
+* Please read host implementation (shared_data.cc/h) and Rust SDK/hostcalls.rs for better understanding.
+**/
+
 // Callout lock is acquired by placing a key-value pair inside shared data.
-// Since, only one thread is allowed to access shared data (host uses mutex) thus only one winner;
 pub fn set_callout_lock(
     root_id: u32,
     context_id: u32,
@@ -23,23 +53,23 @@ pub fn set_callout_lock(
     // check if lock is already acquired or not
     match get_shared_data(&request_key)? {
         (_, None) => {
-            // lock not acquired yet, try to set lock.
-            // Note: Since two threads can get None for CAS at the same time, to avoid
-            // overwriting of lock by another thread, use any value for CAS other than 0.
+            info!(
+                context_id,
+                "thread ({}): trying to acquire lock ({}) the first time", root_id, request_key
+            );
+            // Note: CAS is not 'None' here                              ∨∨∨∨∨∨
             match set_shared_data(&request_key, Some(root_id_str_bytes), Some(1)) {
                 Ok(()) => {
                     info!(
                         context_id,
-                        "thread(id: {}): callout-lock acquired for request(key: {})",
-                        root_id,
-                        request_key
+                        "thread ({}): callout-lock ({}) acquired", root_id, request_key
                     );
                     Ok(true)
                 }
                 Err(Status::CasMismatch) => {
                     info!(
                         context_id,
-                        "thread(id: {}): callout-lock for request(key:{}) already acquired by another thread",
+                        "thread ({}): callout-lock ({}) for already acquired by another thread",
                         root_id,
                         request_key
                     );
@@ -53,7 +83,7 @@ pub fn set_callout_lock(
                 std::str::from_utf8(&bytes).unwrap_or("thread id failed to deserialize");
             info!(
                 context_id,
-                "thread(id: {}): callout-lock for request(key: {}) already acquired by thread(id: {})",
+                "thread ({}): callout-lock ({}) already acquired by thread ({})",
                 root_id,
                 request_key,
                 lock_acquired_by
@@ -63,7 +93,7 @@ pub fn set_callout_lock(
         (None, Some(cas)) => {
             info!(
                 context_id,
-                "thread(id: {}): callout-lock was already free and trying to acquire now for request(key: {})",
+                "thread ({}): callout-lock ({}) was already free and trying to acquire again",
                 root_id,
                 request_key
             );
@@ -71,7 +101,7 @@ pub fn set_callout_lock(
                 Ok(()) => {
                     info!(
                         context_id,
-                        "thread(id: {}): callout-lock acquired for request(key: {})",
+                        "thread ({}): callout-lock ({}) successfully acquired",
                         root_id,
                         request_key
                     );
@@ -80,7 +110,7 @@ pub fn set_callout_lock(
                 Err(Status::CasMismatch) => {
                     info!(
                         context_id,
-                        "thread(id: {}): callout-lock for request(key: {}) already acquired by another thread",
+                        "thread ({}): callout-lock ({}) already acquired by another thread",
                         root_id,
                         request_key
                     );
@@ -94,6 +124,7 @@ pub fn set_callout_lock(
 
 // NOTE: Right now, there is no option of deleting the pair instead only the value can be erased,
 // and it requires changes in the ABI so change this because it will lead to better memory usage.
+// Callout-lock is freed by setting null value in the cache for the request key.
 pub fn free_callout_lock(
     root_id: u32,
     context_id: u32,
@@ -102,15 +133,13 @@ pub fn free_callout_lock(
     let request_key = format!("callout_{}", cache_key.as_string());
     info!(
         context_id,
-        "thread(id: {}): trying to free callout-lock (key: {})", root_id, request_key
+        "thread ({}): trying to free callout-lock ({})", root_id, request_key
     );
 
     if let Err(Status::NotFound) = get_shared_data(&request_key) {
         warn!(
             context_id,
-            "thread(id: {}): trying to free non-existing callout-lock (key: {})",
-            root_id,
-            request_key
+            "thread ({}): trying to free non-existing callout-lock ({})", root_id, request_key
         );
         return Err(Status::NotFound);
     }
@@ -118,7 +147,7 @@ pub fn free_callout_lock(
     if let Err(e) = set_shared_data(&request_key, None, None) {
         warn!(
             context_id,
-            "thread(id: {}): failed to delete the callout-lock (key: {}) from shared data: {:?}",
+            "thread ({}): failed to delete the callout-lock ({}) from shared data: {:?}",
             root_id,
             request_key,
             e
